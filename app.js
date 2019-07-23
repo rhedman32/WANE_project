cb6dcbd5505b41620939e95bddfd3bf74dbeaceb
Ricardo Hedman <RHedman@Masonite.com>
2019-07-23 16:42:44 -0400

    updating packages

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 app.get('/', function(request, response){
     response.sendFile(path.join(__dirname, 'views/index.html'));
