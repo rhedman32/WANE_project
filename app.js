51fa1f7a6b2747422158836865d98c76b919af24
Ricardo Hedman <RHedman@Masonite.com>
2019-07-23 14:40:27 -0400

    setting up more files

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
+var morgan = require('morgan');
+var path = require('path');
 
 var app = express();
 
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
+
 app.get('/', function(request, response){
-    response.send('Hello from my library app');
+    response.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(3000, function(){
